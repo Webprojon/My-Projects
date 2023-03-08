9221e2e9821fb88134c4ce33699808f809333c48
Tokhirjon <tokhirjon0107@gmail.com>
2023-03-08 11:07:33 +0100

    feat: added all folders

---
+++ js/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    name: "My Quote App",
+    proImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ817-Kd6nLtEmtvoLG1YC8LfSpWsbwdy2_Ee49q0NjeDlGh-Sb4UKZ5pE2K4yWdhgLKHk&usqp=CAU",
+    link: "https://webprojon.github.io/my-quote/",
+  },
+  {
+    name: "Online Learning Center",
+    proImg:
+      "https://cdn.pixabay.com/photo/2018/05/19/00/53/online-education-3412473__340.jpg",
+    link: "https://itplanet.netlify.app/",
+  },
+  {
+    name: "Pixel Com Temp",
+    proImg:
+      "https://www.pexels.com/assets/static/images/meta/pexels-stock-photos.jpg",
+    link: "https://webprojon.github.io/Pixel-com/",
+  },
+  {
+    name: "To Do App",
+    proImg:
+      "https://media.istockphoto.com/id/1285308242/photo/to-do-list-text-on-notepad.jpg?s=612x612&w=0&k=20&c=p85bCVQZwvkrqqqNOJGg2QuPDu6ynTlQHkASQOTELh8=",
+    link: "https://webprojon.github.io/My-To-Do-App/",
+  },
+  {
+    name: "Quiz App",
+    proImg:
+      "https://media.istockphoto.com/id/1268465415/photo/quiz-time-concept-speech-bubble-with-pencil-on-yellow-background.jpg?s=612x612&w=0&k=20&c=ZowfYpCJeyknpWhnfyWqV1_If6aJmFUiSqqqEUBhvAg=",
+    link: "https://webprojon.github.io/my-quiz-app/",
+  },
+  {
+    name: "Average Grade Calculation",
+    proImg:
+      "https://thumbs.dreamstime.com/b/colorful-illustration-mathematics-modern-outline-style-college-subject-icon-colorful-illustration-mathematics-110875148.jpg",
+    link: "https://webprojon.github.io/Average-Grade/",
+  },
+  {
+    name: "Easy Contact Form",
+    proImg:
+      "https://newtodesign.com/wp-content/uploads/2018/09/Contact-Form-Style-CSS3.png",
+    link: "https://webprojon.github.io/Easy-Form/",
+  },
+];
